58af7c90eb63d7ee27ba8de180a55b27df74fc1a
Jeeeeeesus <Jeeeeeesus@users.noreply.github.com>
2021-12-03 14:12:52 +0100

    test

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,10 @@
 public class Hello {
-    static String hello = "Hello World!";
+    static String hello = "Hello World!!!";
 
     public static void main(String[] args) {
         System.out.println(hello);
+        
+        
+        
     }
 }
